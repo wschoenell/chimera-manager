528cdce8eb28bb55bc263fb2edcfdb1ea2309a3b
Tiago Ribeiro de Souza <tiago.astro@gmail.com>
2017-02-14 11:11:05 -0300

    Still debugging recurrent algorithm

--- chimera_manager/controllers/scheduler/algorithms.py
+++ chimera_manager/controllers/scheduler/algorithms.py
@@ -1161,9 +1161,9 @@ class Recurrent(BaseScheduleAlgorith):
 
         ntargets = len(kwargs['query'][:])
         # Exclude targets that where observed less then a specified ammount of time
-        kwargs['query'] = kwargs['query'].filter(or_(ObsBlock.observed == False,
-                                                     and_(ObsBlock.observed == True,
-                                                          ObsBlock.lastObservation < reference_date)))
+        # kwargs['query'] = kwargs['query'].filter(or_(ObsBlock.observed == False,
+        #                                              and_(ObsBlock.observed == True,
+        #                                                   ObsBlock.lastObservation < reference_date)))
         new_ntargets = len(kwargs['query'][:])
         log.debug('Filtering %i of %i targets' % (new_ntargets, ntargets))
         # Select targets with the Higher algorithm
